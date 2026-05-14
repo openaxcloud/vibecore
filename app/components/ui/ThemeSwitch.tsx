f34ff620e2e1213cb6855803bec76bc817d4adc1
openaxcloud <simon@openax.com>
2026-05-14 10:15:47 +0300

    Forward refs from ThemeSwitch

--- app/components/ui/ThemeSwitch.tsx
+++ app/components/ui/ThemeSwitch.tsx
@@ -1,5 +1,5 @@
 import { useStore } from '@nanostores/react';
-import { memo, useEffect, useState } from 'react';
+import { forwardRef, memo, useEffect, useState } from 'react';
 import { IconButton } from './IconButton';
 import { themeStore, toggleTheme } from '~/lib/stores/theme';
 
@@ -11,25 +11,30 @@ interface ThemeSwitchProps {
 }
 
 export const ThemeSwitch = memo(
-  ({ className, iconClassName, size = 'xl', title = 'Toggle Theme' }: ThemeSwitchProps) => {
-    const theme = useStore(themeStore);
-    const [domLoaded, setDomLoaded] = useState(false);
+  forwardRef<HTMLButtonElement, ThemeSwitchProps>(
+    ({ className, iconClassName, size = 'xl', title = 'Toggle Theme' }, ref) => {
+      const theme = useStore(themeStore);
+      const [domLoaded, setDomLoaded] = useState(false);
 
-    useEffect(() => {
-      setDomLoaded(true);
-    }, []);
+      useEffect(() => {
+        setDomLoaded(true);
+      }, []);
 
-    return (
-      domLoaded && (
-        <IconButton
-          className={className}
-          icon={theme === 'dark' ? 'i-ph-sun-dim-duotone' : 'i-ph-moon-stars-duotone'}
-          iconClassName={iconClassName}
-          size={size}
-          title={title}
-          onClick={toggleTheme}
-        />
-      )
-    );
-  },
+      return (
+        domLoaded && (
+          <IconButton
+            ref={ref}
+            className={className}
+            icon={theme === 'dark' ? 'i-ph-sun-dim-duotone' : 'i-ph-moon-stars-duotone'}
+            iconClassName={iconClassName}
+            size={size}
+            title={title}
+            onClick={toggleTheme}
+          />
+        )
+      );
+    },
+  ),
 );
+
+ThemeSwitch.displayName = 'ThemeSwitch';
